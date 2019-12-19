63b5aff180443aaa4c6e972cbc913df8af0b3b0a
Myfanwy <mrowlan1@gmail.com>
2019-12-19 12:49:39 -0800

    update stns

--- data-raw/stns.R
+++ data-raw/stns.R
@@ -3,5 +3,5 @@
 stns <- read.csv(system.file("extdata", "stnkms.csv", package = "ybt", mustWork = TRUE), stringsAsFactors = FALSE)
 stns <- stns[!is.na(stns$GroupedStn), ]
 stns <- stns[!duplicated(stns$GroupedStn), c("GroupedStn", "rkms")]
-
+stns <- stns[order(stns$rkms), ]
 usethis::use_data("stns")
