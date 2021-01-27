570ed6df815b0cf578f2ba7b693a1a63bd609ac4
Myfanwy <myfanwy@github.com>
2021-01-26 16:08:23 -0800

    delete get_last_bard function

--- R/bard_functions.R
+++ R/bard_functions.R
@@ -50,16 +50,3 @@ get_last_bd_onefish <- function(bq) {
   return(bqf)
 }
 
-#' get final location and detection time for every tagID in the output of a bard query
-#' @param bdf Dataframe returned by a BARD query, i.e. the return object of `bard_query`. Note - `get_last_bd()` calls `parse_tagid_col()` internally, so don't call that function before you run this one.
-#' @param tagcol Name of the TagID column in `bdf`
-#' @return Dataframe with columns: `TagID`, `last_det_bard` = last detection recorded for that TagID in the provided BARD dataframe, `last_loc_bard` = name of the Station associated with the last detection
-#' @details Note - `get_last_bd` calls `parse_tagid_col()` internally, so don't call that function before you run this one.
-#'
-get_last_bard <- function(bdf, tagcol = "TagID") {
-  #bds = bq # function testing
-  bdf <- parse_tagid_col(bdf, tagcol = tagcol)
-  bdf2 <- by(bdf, bdf$TagID, get_last_bd_onefish)
-  return(do.call(rbind, bdf2))
-}
-
