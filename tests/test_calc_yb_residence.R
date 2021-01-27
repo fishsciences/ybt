1c28e452e2841405dcfd72335d2f9f229776fcc6
Myfanwy <myfanwy@github.com>
2021-01-26 21:02:50 -0800

    remove unfinished test, update R version dependency

--- tests/test_calc_yb_residence.R
+++
@@ -1,8 +0,0 @@
-library(ybt)
-library(tagtales)
-
-load("tag_tale_test_df.rda")
-d = tag_tales(tag_tale_test_df, "TagID", "GroupedStn", "DateTimePST")
-d$ReleaseTime = as.POSIXct("2017-09-01 00:00:00", tz = "Pacific/Pitcairn")
-
-d2 = get_exit_times(d, "TagID")
